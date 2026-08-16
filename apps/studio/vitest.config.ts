5306e9e1008f969ef810fe2cfbf0351eecfb701d
Jeius <pahamajulius@gmail.com>
2026-08-16 11:04:27 +0800

    chore(studio): update vitest configuration to load the .env.test

--- apps/studio/vitest.config.ts
+++ apps/studio/vitest.config.ts
@@ -1,4 +1,11 @@
 import { baseConfig } from '@eduksource/config/vitest';
+import { config } from 'dotenv';
+import { resolve } from 'path';
 import { defineConfig } from 'vitest/config';
 
-export default defineConfig(baseConfig);
+const env = config({ path: resolve(import.meta.dirname, '.env.test') });
+
+export default defineConfig({
+  ...baseConfig,
+  test: { ...baseConfig.test, env: env.parsed, environment: 'node', setupFiles: []},
+});
